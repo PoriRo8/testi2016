9b2960e140a5ccbf1750af5ecfc2f0829ce89f68
PoriRo8 <arto.kauppinen@student.tut.fi>
2016-03-14 10:57:58 +0200

    Yritys

---
+++ testigit/src/testigit/testi.java
@@ -0,0 +1,10 @@
+package testigit;
+
+public class testi {
+	public static void main(String[] args){
+		//public static void main(String[]args)
+	
+	System.out.println("ekaa committia pukkaa");
+	System.out.println("ekaa committia pukkaa MUUTOS");
+	}
+}
