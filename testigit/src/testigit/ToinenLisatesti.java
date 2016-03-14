a6a0100a7ac230c5765b7c1234fc0b49d9afd6c7
PoriRo8 <arto.kauppinen@student.tut.fi>
2016-03-14 11:16:38 +0200

    Jotain

---
+++ testigit/src/testigit/ToinenLisatesti.java
@@ -0,0 +1,18 @@
+package testigit;
+
+public class ToinenLisatesti {
+	
+			
+				public static void main(String[] args){
+				//public static void main(String[]args)
+			
+			System.out.println("ekaa committia pukkaa");
+			System.out.println("ekaa committia pukkaa MUUTOS88");
+			System.out.println("ekaa committia pukkaa MUUTOS8899");
+			
+			}
+		}
+
+
+
+
