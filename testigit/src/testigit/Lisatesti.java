fe5e59371a003cfe933ca4eb406418ab848ec4d5
PoriRo8 <arto.kauppinen@student.tut.fi>
2016-03-14 11:04:54 +0200

    Lisayritys

---
+++ testigit/src/testigit/Lisatesti.java
@@ -0,0 +1,13 @@
+package testigit;
+
+public class Lisatesti {
+	
+			public static void main(String[] args){
+			//public static void main(String[]args)
+		
+		System.out.println("ekaa committia pukkaa");
+		System.out.println("ekaa committia pukkaa MUUTOS88");
+		}
+	}
+
+
