3aca8cd34c5ec8a2b460651fed7b3805609c6cfd
PoriRo8 <arto.kauppinen@student.tut.fi>
2016-03-14 11:24:37 +0200

    KolmasLisatestiluokka

---
+++ testigit/src/testigit/KolmasLisatesti.java
@@ -0,0 +1,16 @@
+package testigit;
+
+public class KolmasLisatesti {
+	
+		
+		
+		public static void main(String[] args){
+		//public static void main(String[]args)
+	
+	System.out.println("ekaa committia pukkaa");
+	System.out.println("ekaa committia pukkaa MUUTOS88");
+	System.out.println("ekaa committia pukkaa MUUTOS88995555");
+	
+	}
+
+}
